1f35f8f5766eb0c7d7ffa0a29f6a2605e857f77c
Kushino-Y <kuskus7856.yi@gmail.com>
2021-03-16 03:08:00 +0000

    task

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -13,8 +13,11 @@ class BooksController < ApplicationController
   
   def create
     book = Book.new(book_params)
-    book.save
-    redirect_to book_path(book.id)
+    if book.save
+      redirect_to book_path(book.id) ,notice:'Book was successfully created.'
+    else
+      redirect_to "/books/new" ,notice:'入力した内容にエラーがあります。'
+    end
   end
 
   def edit
@@ -23,8 +26,11 @@ class BooksController < ApplicationController
   
   def update
     book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path, notice:'Book was successfully created.'
+    if book.update(book_params)
+      redirect_to book_path , notice:'Book was successfully created.'
+    else
+      redirect_to edit_books_path ,notice:'入力した内容にエラーがあります。'
+    end
   end
   
   def destroy
